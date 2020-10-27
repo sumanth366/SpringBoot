284af0c82644c845f02cfe814f18ece54ced2317
DELL <sumanthsonu123@gmail.com>
2020-10-27 19:58:00 +0530

    first commit

---
+++ Desktop/Sum Lea/Alien/Alien/src/main/java/com/sumanth/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.sumanth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sumanth.dao.AlienRepo;
+import com.sumanth.model.Alien;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	AlienRepo repo;
+	
+	
+	@GetMapping("/addAlien")
+	public ModelAndView addAlien(Alien a)
+	{
+		ModelAndView mv= new ModelAndView("home.jsp");
+		mv.addObject(a);
+		repo.save(a);
+		return mv;
+	}
+	
+	@RequestMapping("login")
+	public ModelAndView login(@RequestParam int id)
+	{
+		ModelAndView mv= new ModelAndView("home.jsp");
+		Alien a = repo.findById(id).orElse(new Alien());
+		mv.addObject(a);
+		return mv;
+	}
+
+}
---
+++ Desktop/Sum Lea/Alien/Alien/src/main/webapp/login.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="addAlien" method="get">
+Enter Id: <input type="text" name="id"><br>
+Enter Name: <input type="text" name="name"><br>
+Enter Tech: <input type="password" name="tech"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
